49b007fb98e2764c5bb2fbf72ea8760503622feb
hoanganhtus <ytu0142@gmail.com>
2026-02-13 21:36:24 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="max-w-[295px] w-full mx-auto bg-white h-screen">
+        {/* tú */}
+
+
+        {/* quyn */}
+
+      </div>
+    </div>
+  )
+}
+
+export default App
+
